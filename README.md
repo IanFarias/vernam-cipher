80102a2fdbfcbc2ddc2c42d1a9e193bce2a1af81
Ian Farias Paixão <ianfariaspaixao14@gmail.com>
2024-05-27 00:25:41 -0300

    adding error handling and update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # vernam-cipher
-Vernam cipher algorithm bitwise with Python
+
+Vernam cipher algorithm bitwise XOR with Python 3.11.2
+
+# About
+
+https://adacomputerscience.org/concepts/encrypt_vernam?examBoard=all&stage=all
+https://www.geeksforgeeks.org/vernam-cipher-in-cryptography/
+https://wiki.imesec.ime.usp.br/books/ctf-starter-pack/page/one-time-pad
+
+# Inputs
+
+- A plain text message (\*.txt File)
+- A Key with maximum length of 12 characters
+- Script action (encrypt or decrypt)
+
+In order to facilitate usability, it was defined that only a key with a maximum length of 12 characters can be sent as a parameter. If the plain text message has a longer length, the key is completed with itself until it reaches the length of the plain text message.
+
+# Outputs
+
+- The encrypted file is save in the "output" directory: ./output/file_name_cripto.txt
+- The decrypted file is save in the "output" directory: ./output/file_name_decrypted.txt
+
+# Commands
+
+Run script
+
+```
+python vernam.py <file.txt> "<key>" -a <encrypt | decrypt>
+```
+
+Help
+
+```
+python vernam.py --help
+```
+
+# Example
+
+message.txt = "q2z"
+
+Encrypt
+
+```
+python vernam.py message.txt "#w>" -a encrypt
+
+/*
+ Result: RED
+*/
+```
+
+Decrypt
+
+```
+python vernam.py ./output/message_cripto.txt "#w>" -a decrypt
+
+/*
+ Result: q2z
+*/
+```
--- vernam.py
+++ vernam.py
@@ -3,14 +3,24 @@ import os
 import argparse
 
 def readFile(path):
-    with open(path, 'r', encoding = 'utf-8') as file:
-        contents = file.read()
-
-    return contents
+    try:
+        with open(path, 'r', encoding = 'utf-8') as file:
+            contents = file.read()
+        return contents
+    except FileNotFoundError:
+        print(f"O arquivo {path} não foi encontrado.")
+        exit()
+    except IOError:
+        print(f"Não foi possível ler o arquivo {path}.")
+        exit()
 
 def writeFile(path, content):
-    with open(path, 'w', encoding ='utf-8') as file:
-        file.write(str(content))
+    try:
+        with open(path, 'w', encoding ='utf-8') as file:
+            file.write(str(content))
+    except IOError as error:
+        print(f"Não foi possível escrever no arquivo {path}: {error}")
+
 
 def toBinary(string):
     binaryChars = []
@@ -84,15 +94,19 @@ def decrypt(file_path, key_pattern):
     writeFile( f'./output/{file_name}_decrypted.txt', decrypted_text)
     
 parser = argparse.ArgumentParser(description='Encryption')
-parser.add_argument('filename', nargs='?', default='')
-parser.add_argument('key',  nargs='?',  default='')
-parser.add_argument('-a', '--action',)
+parser.add_argument('filename', nargs='?', default='', help='Caminho do arquivo')
+parser.add_argument('key',  nargs='?',  default='', help='Chave de criptografia (max. 12 caracteres)')
+parser.add_argument('-a', '--action', help='Ação escolhida (encrypt | decrypt)')
 args = parser.parse_args()
 
 def main(args):
     file_name = args.filename
     key = args.key
     action = args.action
+
+    if len(key) > 12:
+        print(f"Chave [ {key} ] precisa ter menos de 12 caracteres.")
+        exit()
 
     if action == 'encrypt':
         encrypt(file_name, key)
